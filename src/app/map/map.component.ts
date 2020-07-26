09cec7d61ba43e3099b4b69116cf9eab3b20773b
AbbasKhalili <khalilikhoo2@gmail.com>
2020-07-26 09:39:29 +0430

    modify map component

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,20 +10,21 @@ import * as L from 'leaflet';
 export class MapComponent implements AfterViewInit {
   map : any;
   
-  tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  });
+  tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	});
 
   constructor() { }
 
   ngAfterViewInit(): void {
-    this.initMap();
-    this.tiles.addTo(this.map);
-  }
-
-  initMap(): void {
     this.map = L.map('map').setView([35.70, 51.35], 13);
+    this.tiles.addTo(this.map);
     this.map.on('click', this.onMapClick);
   }
 
@@ -34,11 +35,53 @@ export class MapComponent implements AfterViewInit {
     L.marker([e.latlng.lat, e.latlng.lng]).addTo(this.map)
 		.bindPopup(e.latlng.toString()).openPopup();
 
-		this.popup
+		/*this.popup
 			.setLatLng(e.latlng)
 			.setContent("You clicked the map at " + e.latlng.toString())
-			.openOn(this.map);
+			.openOn(this.map);*/
 	}
 
 }
+
+/*
+var mymap = L.map('mapid').setView([35.70, 51.35], 13);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+
+	
+	/*L.circle([51.508, -0.11], 500, {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	}).addTo(mymap).bindPopup("I am a circle.");
+
+	L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	]).addTo(mymap).bindPopup("I am a polygon.");
+
+var popup = L.popup();
+
+function onMapClick(e) {
+      debugger;
+  L.marker([e.latlng.lat, e.latlng.lng]).addTo(mymap)
+  .bindPopup(e.latlng.toString()).openPopup();
+
+  popup
+    .setLatLng(e.latlng)
+    .setContent("You clicked the map at " + e.latlng.toString())
+    .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
+*/
 
