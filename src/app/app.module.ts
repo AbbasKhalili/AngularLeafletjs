866577b92e12eb35ca677a10bb0392149a1e76f0
AbbasKhalili <khalilikhoo2@gmail.com>
2020-07-27 12:47:33 +0430

    add datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgPersianDatepickerModule } from 'ng-persian-datepicker';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { SelectDateComponent } from './select-date/select-date.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    SelectDateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgPersianDatepickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
