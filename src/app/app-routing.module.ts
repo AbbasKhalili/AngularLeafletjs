bfaf9130e8812f15e7714adeec381700bef102bd
AbbasKhalili <khalilikhoo2@gmail.com>
2020-07-28 12:33:22 +0430

    add links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MapComponent } from './map/map.component';
+import { SelectDateComponent } from './select-date/select-date.component';
 
 const routes: Routes = [
-  { path: 'map', component: MapComponent }
+  { path: 'map', component: MapComponent },
+  { path: 'datepicker', component: SelectDateComponent }
 ];
 
 @NgModule({
